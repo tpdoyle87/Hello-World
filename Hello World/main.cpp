58ab7172e1b6668c5d92db7b61c1adc682b335c9
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-09-08 19:44:02 -0300

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Thomas Doyle-Engler on 9/8/18.
+//  Copyright © 2018 Thomas Doyle-Engler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
