857ff4ce03b2d26933dbeda120a57637190ce8e5
Thomas DoyleEngler <tpdoyle87@gmail.com>
2019-03-21 19:51:44 -0400

    playing around with c++

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -1,15 +1,9 @@
-//
-//  main.cpp
-//  Hello World
-//
-//  Created by Thomas Doyle-Engler on 9/8/18.
-//  Copyright © 2018 Thomas Doyle-Engler. All rights reserved.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    cout << "Hello there world!" << endl;
+    cout << "i just rebuilt this whole function" << endl;
+    cout << "Go ME!!" << endl;
     return 0;
 }
